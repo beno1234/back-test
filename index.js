1a285bdc0f0701986a1de14b2d0569aecab1738b
beno1234 <benolopesdias@gmail.com>
2023-04-10 10:47:30 -0300

    foi

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Ae@1254453",
-  database: "dogz",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 app.use(express.json());
